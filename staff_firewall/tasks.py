882405f1c51fe5f98813c3b7148f5c9153b39943
Serhii <sera.ua09@gmail.com>
2024-09-12 11:25:02 +0300

    update code

--- staff_firewall/tasks.py
+++ staff_firewall/tasks.py
@@ -3,14 +3,14 @@ from fleio.celery import app
 from fleio.openstack.models import Instance
 
 from .models import FwStaff, FwRegions
-
 from .opnsense import rule_manager
 
 LOG = logging.getLogger(__name__)
 
+
 @app.task(name='Cleanup firewall rule', do_not_create_activity=True)
-def cleanup_firewall_rule():
-    rules  = FwStaff.objects.all()
+def cleanup_firewall_rule() -> None:
+    rules = FwStaff.objects.all()
     for rule in rules:
         try:
             Instance.objects.get(id=rule.instance_id)
